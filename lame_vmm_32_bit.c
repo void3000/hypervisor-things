d788100e3ac36603f8115ebad470737857a11105
void3000 <keorapetse.finger@yahoo.com>
2024-07-26 00:11:34 +0200

    Enable 32 bit paging with 4 Kib pages.

--- lame_vmm_32_bit.c
+++ lame_vmm_32_bit.c
@@ -10,7 +10,7 @@
 #include <linux/kvm.h>			/* KVM_GET_API_VERSION */
 
 #define TSS_ADDRESS			0xfffbd000
-#define VM_MEM_SIZE 			0x100000
+#define VM_MEM_SIZE 			0x8000
 
 #define KVM_PERM			(O_RDWR | O_CLOEXEC)
 #define VM_MEM_PERM			(PROT_EXEC | PROT_READ | PROT_WRITE)
@@ -22,23 +22,27 @@
 
 /* https://wiki.osdev.org/CPU_Registers_x86#CR0 */
 #define CR0_PE	1u
+#define CR0_PG	(1 << 31)
 
 int main()
 {
 	int kvm_fd = open("/dev/kvm", KVM_PERM);
 
 	int vm_fd = ioctl(kvm_fd, KVM_CREATE_VM, 0);
-	/* The TSS is a structure used in x86 architecture to store information
+
+	/* the TSS is a structure used in x86 architecture to store information
 	* about a task, such as processor registers, I/O map base address, and 
 	* stack pointers. It plays a crucial role in handling hardware task 
 	* switching and certain privileged operations.
 	*/
 	ioctl(vm_fd, KVM_SET_TSS_ADDR, TSS_ADDRESS);
+
 	/* Allocate a block of memory in the host's virtual address space for
 	 * our virtual machine (VM).
 	 */
 	void* host_virt_mem = mmap(0, VM_MEM_SIZE, VM_MEM_PERM, VM_MEM_MAP_PERM, -1, 0);
-	/* Inside the guest OS, memory addresses used by applications 
+
+	/* inside the guest OS, memory addresses used by applications 
 	 * are guest virtual addresses (GVA). The guest OS translates 
 	 * these to guest physical addresses (GPA) using its own page 
 	 * tables. KVM maps the guest physical addresses to host 
@@ -62,33 +66,27 @@ int main()
 	}
 
 	int vcpu_fd = ioctl(vm_fd, KVM_CREATE_VCPU, 0);
-	
-	struct kvm_regs vpcu_regs = {
-		.rflags = 0x02
-	};
-	
-	ioctl(vcpu_fd, KVM_SET_REGS, &vpcu_regs);
-	/* Prepare to jump to protected mode by setting up Global Descriptor
-	 * Table (GDT).
+
+	/* setup protected mode by setting up Global Descriptor Table (GDT).
 	 * 
-	 * Helpful information:
+	 * helpful information:
 	 * - https://wiki.osdev.org/Global_Descriptor_Table
 	 * - https://wiki.osdev.org/Segment_Selector
 	 * 
-	 * Protected mode allows for the following:
+	 * protected mode allows for the following:
 	 * 	1. assess to 4 gb of RAM
 	 * 	2. protect certain memory regions
 	 * 	3. set appropriate privilage levels
 	 */
 	struct kvm_sregs vm_sregs;
 	struct kvm_segment vm_seg = {
-		.base = 0x0000,
-		.limit = 0xffff,
 		.selector = 0x00,
-		.type = 0x0b,
-		.present = 0x01,
-		.dpl = 0x00,
-		.db = 0x01,
+		.present  = 0x01,
+		.base 	= 0x0000,
+		.limit 	= 0xffff,
+		.type 	= 0x0b,
+		.dpl 	= 0x00,
+		.db 	= 0x01,
 		.s = 0x01,
 		.l = 0x00,
 		.g = 0x01
@@ -96,7 +94,6 @@ int main()
 
 	ioctl(vcpu_fd, KVM_GET_SREGS, &vm_sregs);
 
-	vm_sregs.cr0 |= CR0_PE; 
 	vm_sregs.cs = vm_seg;
 
 	vm_seg.type = 0x03;
@@ -108,10 +105,35 @@ int main()
 	vm_sregs.gs = vm_seg;
 	vm_sregs.ss = vm_seg;
 
+	/* setup 32 bit paging with 4 Kib pages
+	 * https://de.wikipedia.org/wiki/Paging#32-Bit-Paging
+	 * https://wiki.osdev.org/CPU_Registers_x86#CR0
+	 */
+	unsigned int pgd_addr = 0x1000;
+	unsigned int *pgd = (unsigned int*)(host_virt_mem + pgd_addr);
+
+	/* the page directory and each page table consist of 1024 4-byte entries */
+	unsigned int pg_addr = 0x2000;
+	unsigned int *pg = (unsigned int*)(host_virt_mem + pg_addr);
+
+	pgd[0] = pg_addr | 0x03;
+	pg[0] = 0x03;
+
+	vm_sregs.cr3 = pgd_addr;
+	vm_sregs.cr0 = (CR0_PE | CR0_PG);
+
 	ioctl(vcpu_fd, KVM_SET_SREGS, &vm_sregs);
+
+	struct kvm_regs vpcu_regs = {
+		.rflags = 0x02,
+		.rip = 0x0000
+	};
+
+	ioctl(vcpu_fd, KVM_SET_REGS, &vpcu_regs);
 
 	int vcpu_mmap_size = ioctl(kvm_fd, KVM_GET_VCPU_MMAP_SIZE, (void*) 0);
 	void* vcpu_addr = mmap(0, vcpu_mmap_size, VCPU_MEM_PERM, VCPU_MEM_MAP_PERM, vcpu_fd, 0);
+	
 	struct kvm_run *vm_state = (struct kvm_run *) vcpu_addr;
 
 	/* 32 bit program */
@@ -147,7 +169,7 @@ int main()
 					goto exit;
 			}
 		default:
-			printf("Unknown KVM state...\n");
+			printf("Unknown KVM (%d) state...\n", vm_state->exit_reason);
 			goto exit;
 		}
 	}
